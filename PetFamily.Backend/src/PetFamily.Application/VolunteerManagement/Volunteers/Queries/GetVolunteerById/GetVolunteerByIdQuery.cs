0b909d11cd020835631f09e3d9707cec2c828ad6
Alisher <baiyskulov.alisher@gmail.com>
2024-10-06 23:38:33 +0700

    Developed a feature for obtaining a volunteer by ID

---
+++ PetFamily.Backend/src/PetFamily.Application/VolunteerManagement/Volunteers/Queries/GetVolunteerById/GetVolunteerByIdQuery.cs
@@ -0,0 +1,5 @@
+﻿using Application.Abstraction;
+
+namespace Application.VolunteerManagement.Volunteers.Queries.GetVolunteerById;
+
+public record GetVolunteerByIdQuery(Guid Id) : IQuery;
---
+++ PetFamily.Backend/src/PetFamily.Application/VolunteerManagement/Volunteers/Queries/GetVolunteerById/GetVolunteerByIdService.cs
@@ -0,0 +1,32 @@
+﻿using Application.Abstraction;
+using Application.Database;
+using Application.Dtos;
+using Application.Extensions;
+using Domain.Shared;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.VolunteerManagement.Volunteers.Queries.GetVolunteerById;
+
+public class GetVolunteerByIdService(
+    IReadDbContext readDbContext,
+    IValidator<GetVolunteerByIdQuery> validator)
+    : IQueryService<VolunteerDto, GetVolunteerByIdQuery>
+{
+    public async Task<Result<VolunteerDto>> ExecuteAsync(GetVolunteerByIdQuery query,
+        CancellationToken cancellationToken = default)
+    {
+        var validationResult = await validator.ValidateAsync(query, cancellationToken);
+        if (validationResult.IsValid == false)
+            return validationResult.ToErrorList();
+        
+        var volunteerQuery = readDbContext.Volunteers;
+
+        var volunteerResult = await volunteerQuery
+            .FirstOrDefaultAsync(v => v.Id == query.Id, cancellationToken);
+        if (volunteerResult is null)
+            return Errors.General.NotFound($"volunteer with id: {query.Id}");
+
+        return volunteerResult;
+    }
+}
