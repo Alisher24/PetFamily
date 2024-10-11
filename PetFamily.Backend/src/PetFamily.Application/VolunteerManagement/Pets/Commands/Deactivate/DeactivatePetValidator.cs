1ab96c01f3d00d17079d29075aa83f681ddb668f
Alisher <baiyskulov.alisher@gmail.com>
2024-10-11 11:27:56 +0700

    Developed a feature for deactivating a pet by id

---
+++ PetFamily.Backend/src/PetFamily.Application/VolunteerManagement/Pets/Commands/Deactivate/DeactivatePetValidator.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Shared;
+using FluentValidation;
+
+namespace Application.VolunteerManagement.Pets.Commands.Deactivate;
+
+public class DeactivatePetValidator : AbstractValidator<DeactivatePetCommand>
+{
+    public DeactivatePetValidator()
+    {
+        RuleFor(s => s.VolunteerId).NotEmpty().WithError(Errors.General.ValueIsInvalid("VolunteerId"));
+        RuleFor(s => s.PetId).NotEmpty().WithError(Errors.General.ValueIsInvalid("PetId"));
+    }
+}
