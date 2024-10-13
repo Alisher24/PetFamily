4d9a0adc7379fb71b78095f3021e506d483538ba
Alisher <baiyskulov.alisher@gmail.com>
2024-10-13 19:47:17 +0700

    Developed a feature for getting all pets with pagination, filtering and sorting

---
+++ PetFamily.Backend/src/PetFamily.API/Controllers/Pets/PetsController.cs
@@ -0,0 +1,22 @@
+﻿using Application.VolunteerManagement.Pets.Queries.GetPetsWithPaginationAndFiltering;
+using Microsoft.AspNetCore.Mvc;
+using PetFamily.API.Controllers.Pets.Requests;
+using PetFamily.API.Extensions;
+
+namespace PetFamily.API.Controllers.Pets;
+
+public class PetsController : ApplicationController
+{
+    [HttpGet]
+    public async Task<ActionResult> GetAll(
+        [FromQuery] GetPetsWithPaginationAndFilteringRequest request,
+        [FromServices] GetPetsWithPaginationAndFilteringService service,
+        CancellationToken cancellationToken)
+    {
+        var result = await service.ExecuteAsync(request.ToQuery(), cancellationToken);
+        if (result.IsFailure)
+            return result.ErrorList.ToResponse();
+        
+        return Ok(result.Value);
+    }
+}
